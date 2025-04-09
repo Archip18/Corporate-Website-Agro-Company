59d8bcd39f9bc28101688ddf970a1136d145d89b
Archip <gamest200606@gmail.com>
2025-04-09 22:15:35 +0300

    Компіляція SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
+const sass = require('gulp-sass')(require('sass'));
 
 const fileIncludeSetting = {
     prefix: '@@',
     basepath:'@file',
-}
+};
 
 
 
 gulp.task('includeFiles', function(){
     return gulp.
          src('./src/*.html')
-        .pipe(fileInclude(fileIncludeSetting))
-        .pipe(gulp.dest('./dist'))
+         .pipe(fileInclude(fileIncludeSetting))
+         pipe(gulp.dest('./dist/'))
 });
 
+gulp.task('sass',function(){
+    return gulp.src('./src/scss/*.scss')
+     .pipe(sass())
+     .pipe(gulp.dest('./dist/css/'))
+
+})
